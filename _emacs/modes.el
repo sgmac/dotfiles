c16f0e7c6efad6a878dffe1c719b01eed6df3a15
Sergio Galvan <sergalma@gmail.com>
2012-03-30 12:04:44 +0000

    Modifed Emacs modes

--- _emacs/modes.el
+++ _emacs/modes.el
@@ -1,60 +1,84 @@
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  I copied some parts from other people, and I  
 ;;
+;; Alvaro Bautista's configuration
+;;  - https://github.com/alvarobp/dotfiles (Rake..)
+;;
+;; Karl Fogelmark (really nice emacs configuration)
+;;  - Lusty for file and buffer browser.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
 ;;;; Default mode
 ;;
 (add-hook 'text-mode-hook
 	  (function (lambda ()
 		      (turn-on-auto-fill))))
 
+;;
+;;;; Ruby Mode
+;;
+
+(add-to-list 'load-path "~/.emacs.d/modes/ruby-mode")
+(add-to-list 'auto-mode-alist '("\\.rb\\'" . ruby-mode))
+(autoload 'ruby-mode "ruby-mode" "Major mode for Ruby" t)
+
+
+
+;; Rake files are ruby, too, as are gemspecs, rackup files, and gemfiles.
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Guardfile$" . ruby-mode))
+(add-hook 'ruby-mode-hook (lambda () (local-set-key "\r" 'newline-and-indent)))
+
+;; IRB inside Emacs
+(require 'inf-ruby)
+
+;; Ruby electric
+(require 'ruby-electric)
+;(ruby-eletric-mode t)
 
 
 ;;
 ;;;; Plugins
 ;;
 
-;(add-to-list 'load-path "~/.emacs.d/plugins/yasnippet")
-;(require 'yasnippet) ;; not yasnippet-bundle
-;(yas/initialize)
-;(yas/load-directory "~/.emacs.d/plugins/yasnippet/snippets")
-
-  
-
-;; MULTITERM
-;(add-to-list 'load-path "~/.emacs.d/plugins/multiterm")
-;(require 'multi-term)
-;(setq multi-term-program "/bin/bash")
-
-;; ANYTHING
-
-;(add-to-list 'load-path "~/.emacs.d/plugins/anything")
-;(require 'anything-config)
-;(require 'anything-show-completion)
-
-;; NAVIGATION
-
-;(add-to-list 'load-path "~/.emacs.d/emacs-nav")
-;(require 'nav)
+(add-to-list 'load-path "~/.emacs.d/plugins/yasnippet")
+(require 'yasnippet)
+(yas/global-mode 1)
 
 
-;; LUSTY
+;; Multiterm
+(add-to-list 'load-path "~/.emacs.d/plugins/multiterm")
+(require 'multi-term)
+(setq multi-term-program "/bin/bash")
 
-;(add-to-list 'load-path "~/.emacs.d/plugins/lusty")
+
+;; Navigation
+(add-to-list 'load-path "~/.emacs.d/plugins/nav")
+(require 'nav)
+
+
+;; Lusty
+(add-to-list 'load-path "~/.emacs.d/plugins/lusty")
+
+
+(when (require 'lusty-explorer nil 'noerror) ;;Only read if package available
+  ;; override the normal file-opening, buffer switching
+  (global-set-key (kbd "C-x C-f") 'lusty-file-explorer)
+  (global-set-key (kbd "C-x b")   'lusty-buffer-explorer))
 ;(require 'lusty-explorer)
 
 
-
-
-(setq load-path (append load-path (list
-				   "~/.el"
-				   )))
-
-(setq default-frame-alist 
-      '((cursor-color . "yellow")
-	(background-color . "black")
-	(foreground-color . "white"))
-)
-
-
+(add-to-list 'load-path "~/.emacs.d/plugins/colortheme")
+(add-to-list 'load-path "~/.emacs.d/plugins/colortheme/color-theme.el")
+(add-to-list 'load-path "~/.emacs.d/plugins/colortheme/themes")
+(require 'color-theme)
+(color-theme-initialize)
+(color-theme-subdued)
 
 
 (require 'ido)
