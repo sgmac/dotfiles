3dbf7b7a8ca161df193108111f38bf3a3cf2953c
Sergio Galvan <sergalma@gmail.com>
2012-08-19 17:24:17 +0200

    Added script install.rb

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 ### Dotfiles
 
-This repository holds the configuration files for the apps I work with. 
+These are my dotfiles. I've updated the old bash script with one more dynamic in ruby. I keep files for console and desktop. Basically the _install.rb_  does links to the _default_ files. In order to install _desktop_ config files (what includes console as well) :
+
+```bash
+ $ ruby install.rb  desktop
+```
+
+You can also unlink all the files under _ENV['HOME']_:
+```bash
+$ ruby install.rb unlink
+```
+
+If you want to add more dotfiles, drop the files in either default or desktop directories. On the top directory level, you will see folders such as _emacs.d_ or _vim_. By default I drop here the directories that should be linked to the topof my $HOME and they are not dependent neither on console nor desktop.
+
---
+++ install.rb
@@ -0,0 +1,82 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+require 'fileutils'
+require 'pathname'
+
+ROOT_DOTS = File.expand_path(File.dirname(__FILE__))
+
+# Colors idea from http://blog.sosedoff.com/
+class String
+    # colorize functions
+    def red; colorize(self, "\e[1m\e[31m");   end
+    def green; colorize(self, "\e[1m\e[32m"); end
+    def dark_green; colorize(self, "\e[32m"); end
+    def yellow; colorize(self, "\e[1m\e[33m");end
+    def blue; colorize(self, "\e[1m\e[34m");  end
+    def dark_blue; colorize(self, "\e[34m");  end
+    def pur; colorize(self, "\e[1m\e[35m");   end
+    def colorize(text, color_code) "#{color_code}#{text}\e[0m" ; end
+end
+
+def unlink_all
+  puts "Unlinking all files in #{ENV['HOME']}".red
+  Dir["#{ENV['HOME']}/{*,.*}"].each do |file|
+    File.unlink(file) if File.symlink?(file)
+  end
+end
+
+def link_files(dotfiles)
+  msg = ""
+  dotfiles.each do |orig|
+    color = "green"
+    # Link content to Desktop
+    if orig.count("/") > 1
+      dest =  File.join(ENV['HOME'], ".#{orig.sub("desktop/","")}")
+      parent = Pathname.new(dest).parent
+      unless File.exist?(parent)
+        FileUtils.mkdir_p(parent)
+      end
+    else
+      dest = File.join(ENV['HOME'], ".#{File.basename(orig)}" )
+    end
+    if File.exists?(dest)
+      begin
+        File.unlink(dest)
+      rescue Errno::EISDIR => e
+        FileUtils.rm_rf(dest)
+      end
+    end
+    File.symlink("#{ROOT_DOTS}/#{orig}","#{dest}")
+    puts "Link to "  + ".#{orig}#{msg}".send(color.to_s)
+  end
+end
+
+# Get directories and link
+dotfolds = Dir["**"].reject {|file| ! File.directory?(file) }.sort
+
+%w{ default desktop}.each do |dir|
+  dotfolds.delete(dir)
+end
+
+default = Dir["default/*"]
+desktop = Dir["desktop/**/*"].reject { |file|  File.directory?(file) }
+
+unless ARGV[0].nil?
+option = ARGV[0].downcase
+  case option
+  when "desktop"
+    link_files desktop
+  when "unlink"
+    unlink_all
+    exit 0
+  end
+end
+
+link_files dotfolds
+link_files default
+
+
+
+
+
