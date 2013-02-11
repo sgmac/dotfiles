e00db27f9e1d944b72fee85179ecc4da6d1c547a
Sergio Galvan <sergalma@gmail.com>
2013-02-11 22:34:56 +0100

    Desktop linking working out

--- install.py
+++ install.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 import os
-
+import sys
 
 DEFAULT = os.path.abspath('default')
-
-HOME = os.getent('HOME')
+DESKTOP = 'desktop'
+HOME = os.getenv('HOME')
 
 
 def link_default():
@@ -13,9 +13,34 @@ def link_default():
         source = os.path.join(DEFAULT, file)
         dest = os.path.join(HOME, '.%s' % file)
         print 'Symlink to .%s' % file
-        os.symlink(source, dest)
+        try:
+            os.symlink(source, dest)
+        except OSError:
+            continue
+
+
+def link_desktop():
+    for dirname, dirnames, filenames in os.walk(DESKTOP):
+        for file in filenames:
+            source = os.path.join(dirname, file)
+            source = os.path.abspath(source)
+            if 'config' in source:
+                dest_path = os.path.join(HOME, '.%s' % dirname.replace('desktop/', ''))
+                if not os.path.exists(dest_path):
+                    os.makedirs(dest_path)
+                dest = os.path.join(dest_path, file)
+            else:
+                dest = os.path.join(HOME, '.%s' % file)
+            print 'Linking %s' % file
+
+            try:
+                os.symlink(source, dest)
+            except OSError:
+                continue
 
 
 if  __name__ == '__main__':
-    print 'Installing dotfiles\n'
-    link_default()
+    if len(sys.argv) > 1 and sys.argv[1] == 'desktop':
+        link_desktop()
+    else:
+        link_default()
