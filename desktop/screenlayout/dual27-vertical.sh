fc0b3010968e1ffc7a10589528b8a48bcbb8c3e1
Sergio Galvan <sergalma@gmail.com>
2018-07-20 16:31:45 +0200

    (i3): work with different displays

---
+++ desktop/screenlayout/dual27-vertical.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --off --output DP1 --mode 2560x1440 --pos 2560x0 --rotate left --output HDMI2 --off --output HDMI1 --off --output DP2 --mode 2560x1440 --pos 0x0 --rotate normal
